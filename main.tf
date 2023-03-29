8c013299fe09997c5288ad45d3b9d44a7f69cc64
cristo-orexe <ccabrera@orexe.pe>
2023-03-29 13:41:18 -0500

    changes

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 resource "aws_instance" "web" {
-  ami             = "ami-0dfcb1ef8550277af"
+  ami             = "ami-00c39f71452c08778"
   instance_type   = "t2.micro"
   tags = {
     Name = "custom-web"
